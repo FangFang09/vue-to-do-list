da0454651c67db1656403616fe2f74623ce34349
fangfang <hyf781222@gmail.com>
2025-06-23 16:48:58 +0800

    feat: add routes for my-tasks, in-progress, and completed views

--- Vuejs/todolist/src/router/index.js
+++ Vuejs/todolist/src/router/index.js
@@ -1,18 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView,
+      name: 'my-tasks',
+      component: () => import('../views/MyTasksView.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue'),
+      path: '/in-progress',
+      name: 'in-progress',
+      component: () => import('../views/InProgressView.vue'),
+    },
+    {
+      path: '/completed',
+      name: 'completed',
+      component: () => import('../views/CompletedView.vue'),
     },
   ],
 })
