159ab6eaba40595fca6c2bfe1b85e09423bc18b3
fangfang <hyf781222@gmail.com>
2025-06-27 15:27:22 +0800

    feat: create AddTaskInput.vue component to trigger form expansion

---
+++ Vuejs/todolist/src/components/AddTaskInput.vue
@@ -0,0 +1,62 @@
+<script setup>
+const emit = defineEmits(['expand'])
+
+function handleClick() {
+  emit('expand')
+}
+</script>
+
+<template>
+  <div class="input-event" @click="handleClick">
+    <label for="addTask">
+      <i class="fa-regular fa-plus"></i>
+    </label>
+    <input type="text" id="addTask" placeholder="Add Task" />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.input-event {
+  display: flex;
+  justify-content: center;
+
+  position: relative;
+  border-radius: 5px;
+  margin-bottom: 24px;
+
+  input[type='text'] {
+    border: 2px solid $grey-3;
+    width: 80%;
+    padding: 19px 0 18px 67px;
+    border-radius: 5px;
+    font-size: 24px;
+
+    @include deviceScreen($sm-size) {
+      width: 100%;
+    }
+
+    &::placeholder {
+      @include placeholder;
+      font-size: 20px;
+      color: $grey-3;
+    }
+
+    &:focus {
+      outline: 3px solid $primary;
+    }
+  }
+
+  .fa-plus {
+    position: absolute;
+    color: $grey-3;
+    font-size: 24px;
+    top: 50%;
+    left: 75px;
+    transform: translateY(-50%);
+
+    @include deviceScreen($sm-size) {
+      left: 32px;
+    }
+  }
+}
+</style>
