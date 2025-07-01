3d12f48c1d5f78b81e03783fe96cf2a50b15dc4d
fangfang <hyf781222@gmail.com>
2025-07-01 10:43:59 +0800

    feat: create TaskFooter component with styles

---
+++ Vuejs/todolist/src/components/TaskFooter.vue
@@ -0,0 +1,71 @@
+<script setup>
+const emit = defineEmits(['onCancel'])
+
+function onCancel() {
+  emit('onCancel')
+}
+</script>
+
+<template>
+  <div class="task-footer">
+    <div class="button-group">
+      <button type="reset" class="cancel-button" @click="onCancel">
+        <i class="fa-solid fa-xmark"></i>
+        <span>cancel</span>
+      </button>
+      <button type="submit" class="add-button">
+        <i class="fa-solid fa-plus"></i>
+        <span>add task</span>
+      </button>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+// task-footer
+.task-footer {
+  .button-group {
+    display: flex;
+    align-items: center;
+  }
+
+  .cancel-button,
+  .add-button {
+    @include button-setting($black);
+  }
+
+  .cancel-button {
+    background-color: $white;
+    border-radius: 0 0 0 5px;
+
+    &:hover {
+      background-color: $hover-white;
+    }
+
+    .fa-xmark {
+      color: $cancel;
+    }
+
+    > span {
+      color: $cancel;
+    }
+  }
+
+  .add-button {
+    background-color: $primary;
+    border-radius: 0 0 5px 0;
+
+    &:hover {
+      background-color: $hover-primary;
+    }
+
+    .fa-plus {
+      color: $white;
+    }
+
+    span {
+      color: $white;
+    }
+  }
+}
+</style>
