d3b9175175bd1eb068995858f4eba3c6583b5b7d
fangfang <hyf781222@gmail.com>
2025-07-01 10:43:07 +0800

    feat: create TaskHeader component with styles

---
+++ Vuejs/todolist/src/components/TaskHeader.vue
@@ -0,0 +1,87 @@
+<script setup>
+import { computed } from 'vue'
+const props = defineProps({
+  modelValue: String,
+  isPin: Boolean,
+})
+
+const title = computed({
+  get: () => props.modelValue,
+  set: (value) => emit('update:modelValue', value),
+})
+
+const emit = defineEmits(['update:modelValue', 'handleTogglePin'])
+
+function onTogglePin() {
+  emit('handleTogglePin')
+}
+</script>
+<template>
+  <div class="task-header" :class="{ active: isPin }">
+    <input type="checkbox" />
+    <div class="task-input-title">
+      <input type="text" placeholder="Type Something Here" v-model="title" />
+      <i
+        class="fa-star"
+        :class="[isPin ? 'fa-solid active' : 'fa-regular']"
+        @click="onTogglePin"
+      ></i>
+      <i class="fa-solid fa-pen"></i>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.task-header {
+  @include task-header;
+  background-color: $grey-1;
+  border-bottom: 3px solid $grey-3;
+
+  &.active {
+    background: $pin-bg;
+  }
+}
+
+.task-input-title {
+  display: flex;
+  align-items: center;
+  gap: 32px;
+  flex-grow: 1;
+
+  // type something here
+  input[type='text'] {
+    border: none;
+    background-color: transparent;
+
+    flex: 1 1 0;
+    width: 50%;
+
+    &:focus {
+      outline: none;
+    }
+
+    @include title($black);
+
+    @include deviceScreen($sm-size) {
+      @include subtitle;
+    }
+  }
+}
+
+.fa-star {
+  font-size: 24px;
+  color: $grey-5;
+  transition: color 0.2s;
+  cursor: pointer;
+
+  &.active {
+    color: $pin;
+  }
+}
+
+.fa-pen {
+  font-size: 24px;
+  color: $primary;
+  cursor: pointer;
+}
+</style>
