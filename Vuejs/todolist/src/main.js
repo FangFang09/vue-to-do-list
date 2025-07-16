6a3f6ed1044af90cb3883515df845688aebcc9cb
fangfang <hyf781222@gmail.com>
2025-07-16 18:08:46 +0800

    refactor: apply reset styles early by importing _reset.scss in main.js and remove unused all.scss

--- Vuejs/todolist/src/main.js
+++ Vuejs/todolist/src/main.js
@@ -6,7 +6,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import '@/styles/all.scss'
+import './styles/base/_reset.scss'
 
 const app = createApp(App)
 
