616ad1031fd28c51a9306c149a85f70c132a3d71
fangfang <hyf781222@gmail.com>
2025-08-10 22:31:48 +0800

    feat: add delete dialog with confirm and cancel actions

--- Vuejs/todolist/src/components/TaskItem.vue
+++ Vuejs/todolist/src/components/TaskItem.vue
@@ -1,8 +1,10 @@
 <script setup>
-import { computed } from 'vue'
+import { computed, ref } from 'vue'
 import { useTaskStore } from '@/stores/taskStore'
+import { useLoadingStore } from '@/stores/loadingStore'
 
 const taskStore = useTaskStore()
+const loadingStore = useLoadingStore()
 
 const props = defineProps({
   todo: Object,
@@ -20,6 +22,35 @@ function togglePin() {
   if (taskStore.isUpdatingSet.has(props.todo.id)) return
   emit('togglePin', props.todo)
 }
+
+const isShowDialog = ref(false)
+function openDialog() {
+  isShowDialog.value = !isShowDialog.value
+}
+
+async function cancelDelete() {
+  try {
+    loadingStore.startLoading('Processing...')
+    await taskStore.fetchTasks()
+    isShowDialog.value = false
+  } catch (error) {
+    console.log(error.message)
+  } finally {
+    loadingStore.stopLoading()
+  }
+}
+
+async function confirmDelete(taskId) {
+  try {
+    loadingStore.startLoading('Processing...')
+    await taskStore.deleteTask(taskId)
+    isShowDialog.value = false
+  } catch (error) {
+    console.log(error)
+  } finally {
+    loadingStore.stopLoading()
+  }
+}
 </script>
 <template>
   <div class="task-item" :class="{ active: todo.isPin }">
@@ -31,6 +62,18 @@ function togglePin() {
         @change="emit('toggleCompleted', todo)"
       />
       <input type="text" :value="todo.title" :class="{ active: todo.isCompleted }" disabled />
+      <i class="fa-regular fa-trash-can" @click="openDialog"></i>
+
+      <!-- check delete dialog -->
+      <div class="dialog-overlay" v-if="isShowDialog">
+        <div class="dialog-container">
+          <p>Are you sure you want to delete this task?</p>
+          <div class="dialog-action">
+            <button class="cancelDelete" @click="cancelDelete">No</button>
+            <button class="confirmDelete" @click="confirmDelete(props.todo.id)">Yes</button>
+          </div>
+        </div>
+      </div>
       <i
         class="fa-star"
         :class="[todo.isPin ? 'fa-solid active' : 'fa-regular']"
@@ -149,12 +192,96 @@ function togglePin() {
       margin-left: 32px;
     }
   }
+  .fa-trash-can {
+    font-size: 16px;
+    margin-left: 20px;
+    color: $grey-5;
+    cursor: pointer;
+
+    @include deviceScreen($sm-size) {
+      font-size: 24px;
+      margin-left: 32px;
+    }
+  }
 }
 
 .task-status {
   margin-left: 40px;
   > span {
     margin-right: 16px;
+  }
+}
+
+// dialog
+.dialog-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  background-color: rgba($black, 0.4);
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  z-index: 100;
+}
+.dialog-container {
+  background: $white;
+  border-radius: 16px;
+  padding: 20px;
+  margin: 0 20px;
+  max-width: 400px;
+  width: 100%;
+  box-shadow: 0 20px 40px rgba(0, 0, 0, 0.1);
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 12px;
+
+  p {
+    font-size: 19px;
+    margin-bottom: 24px;
+    font-weight: 500;
+  }
+}
+
+.dialog-action {
+  width: 90%;
+  display: flex;
+  gap: 15px;
+
+  button {
+    flex: 1 1 0;
+    padding: 14px 20px;
+    border-radius: 10px;
+    border: none;
+    font-size: 16px;
+    font-weight: 600;
+    cursor: pointer;
+    transition: all 0.2s;
+  }
+}
+
+.cancelDelete {
+  background: $grey-2;
+  color: $grey-5;
+
+  &:hover {
+    background: $grey-3;
+  }
+}
+
+.confirmDelete {
+  background: $cancel;
+  color: white;
+
+  &:hover {
+    background: $cancel;
+    transform: translateY(-1px);
   }
 }
 </style>
